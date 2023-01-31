6e333bcc771a512c0c25cdd8bf3a853b9d295d95
Code-with-jaycee <jayceecoder@gmail.com>
2023-01-31 09:51:25 +0300

    updated

--- Main.r
+++ Main.r
@@ -1,6 +1,6 @@
 x <- c(21, 62, 10, 53)
 labels <- c("London", "New York", "singapore", "Mumbai")
-png(file = "D:/Projects/Data-Science-Machine-Learning-Data-Analysis-Python-R/imagescity.jpg")
+png(file = "D:/Projects/Data-Science-Machine-Learning-Data-Analysis-Python-R/images/imagescity.jpg")
 
 pie(x, labels )
 dev.off()
